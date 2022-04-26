d21bff270dd8cc502868333728654b0b9ff5303f
tylerhanks <teh.hanks@gmail.com>
2022-04-26 11:27:41 -0400

    working on oapply for neural nets

--- src/NAS.jl
+++ src/NAS.jl
@@ -2,8 +2,22 @@ using Catlab.Present
 using Catlab.WiringDiagrams
 using Catlab.Programs
 using Catlab.Theories
+using Catlab.Graphics
+using Catlab.Graphics.Graphviz
 import Catlab.WiringDiagrams: oapply
 using Flux
+
+show_diagram(d::WiringDiagram) = to_graphviz(
+    add_junctions(d),
+    orientation=LeftToRight,
+    labels=false, label_attr=:xlabel,
+    node_attrs=Graphviz.Attributes(
+        :fontname => "Courier",
+    ),
+    edge_attrs=Graphviz.Attributes(
+        :fontname => "Courier",
+    )
+)
 
 @present Primitives(FreeCartesianCategory) begin
     T::Ob
@@ -59,10 +73,47 @@ end=#
 net = @program Primitives (x::T) begin
     top_net = MaxPool3x3(SepConv3x3(DepConv3x3(x)))
     bot_net = MaxPool3x3(Conv1x1(x))
-    return [top_net, bot_net]
+    y = [top_net, bot_net]
+    return AvgPool3x3(y)
 end
 #net = pair(net...)
 
-#function oapply(d::WiringDiagram, gens::AbstractDict{S,L}) where {S, L <: Layer}
+function nearest_common_neighbor(d::WiringDiagram, x::Int, y::Int)
+    
+
+function oapply_nn(d::WiringDiagram, gens::Dict)
+    function parallel_helper(ids)
 
 
+    function rec_helper(id)
+        if length(outneighbors(d, id)) > 1
+            Flux.Parallel(hcat, )
+
+function oapply_nn(d::WiringDiagram, gens::Dict)
+    function rec_helper(id, res)
+        if length(outneighbors(d, id)) > 1
+            for n in outneighbors(d, id)
+                v = Vector()
+                push!(res, v)
+                rec_helper(n, v)
+            end
+        else
+            for n in outneighbors(d, id)
+                @assert haskey(gens, boxes(d)[id].value)
+                push!(res, gens[boxes(d)[id].value])
+                rec_helper(n, res)
+            end
+        end
+    end
+    res = Any[]
+    rec_helper(input_id(d), res)
+    return res
+end
+
+gens = Dict(
+    :DepConv3x3 => 1,
+    :SepConv3x3 => 2,
+    :MaxPool3x3 => 3,
+    :Conv1x1 => 4,
+    :AvgPool3x3 => 5
+)
