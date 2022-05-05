df391eb10fc29b4bf5ce956da891ac90a9c4ba36
tylerhanks <teh.hanks@gmail.com>
2022-05-05 16:22:53 -0400

    example of graph pullbacks
    Co-authored-by: Kris Brown <ksb@stanford.edu>

--- src/NAS.jl
+++ src/NAS.jl
@@ -1,13 +1,63 @@
 using Catlab.Present
 using Catlab.WiringDiagrams
+using Catlab.CategoricalAlgebra
 using Catlab.Programs
+using Catlab.Graphs
 using Catlab.Theories
 using Catlab.Graphics
-using Catlab.Graphics.Graphviz
+import Catlab.Graphics.Graphviz.to_graphviz
+#using Catlab.Graphics.Graphviz
 import Catlab.WiringDiagrams: oapply
 using Flux
 
-show_diagram(d::WiringDiagram) = to_graphviz(
+base = @acset Graph begin
+    V = 3
+    E = 5
+    src = [1,2,2,1,3]
+    tgt = [2,2,3,1,3]
+end
+g = to_graphviz(base)
+
+wide_boi = @acset Graph begin
+    V = 5
+    E = 11
+    src = [1,1,1,2,3,4, 1, 2, 3, 4, 5]
+    tgt = [2,3,4,5,5,5, 1, 2, 3, 4, 5]
+end
+wg = to_graphviz(wide_boi)
+two = @acset Graph begin
+    V = 2
+    E = 2
+    src = [1,2]
+    tgt = [1,2]
+end
+length_boi = @acset Graph begin
+    V = 4
+    E = 7
+    src = [1,2,3, 1, 2 , 3 , 4]
+    tgt = [2,3,4, 1 , 2 , 3 , 4]
+end
+lg = to_graphviz(length_boi)
+
+h1 = homomorphisms(length_boi, base)[2]
+h2 = homomorphisms(wide_boi, base)[2]
+h3 = homomorphisms(two, base)[5]
+
+p = apex(pullback(h1,h2))
+pg = to_graphviz(p)
+
+if false 
+
+#test = apex(coproduct(wide_boi, length_boi))
+#testg = to_graphviz(test)=#
+
+A, B, X, Y = Ob(FreeTracedMonoidalCategory, :A, :B, :X, :Y)
+f = Hom(:f, otimes(X,A), otimes(X,B))
+
+#base = trace(X, A, B, f)
+#bd = Graphviz.to_graphviz(base)
+
+show_diagram(d::WiringDiagram) = Graphviz.to_graphviz(
     add_junctions(d),
     orientation=LeftToRight,
     labels=false, label_attr=:xlabel,
@@ -27,6 +77,13 @@ show_diagram(d::WiringDiagram) = to_graphviz(
     MaxPool3x3::Hom(T, T)
     AvgPool3x3::Hom(T, T)
 end
+
+chain = @program Primitives (x::T) begin
+    return Conv1x1(AvgPool3x3(SepConv3x3(DepConv3x3(x))))
+end
+chain_exp = to_hom_expr(FreeTracedMonoidalCategory, chain)
+
+#h1 = homomorphisms(chain_exp, base)
 
 struct NNDom
     nchannels::Vector{Int} # records how many channels for each layer in a parallel net
@@ -70,7 +127,7 @@ end
     end
 end=#
 
-net = @program Primitives (x::T) begin
+#=net = @program Primitives (x::T) begin
     top_net = MaxPool3x3(SepConv3x3(DepConv3x3(x)))
     bot_net = MaxPool3x3(Conv1x1(x))
     y = [top_net, bot_net]
@@ -116,4 +173,5 @@ gens = Dict(
     :MaxPool3x3 => 3,
     :Conv1x1 => 4,
     :AvgPool3x3 => 5
-)
+)=#
+end
