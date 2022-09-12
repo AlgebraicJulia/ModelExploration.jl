f227125efa24e3936e2dde862891e554479baa77
Tyler Hanks <teh.hanks@gmail.com>
2022-09-12 16:11:36 -0400

    Split petri code into new repo

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,5 @@
 using Test
 
-@testset "param_est" begin
-  include("param_est.jl")
-end
-
-@testset "ttest" begin
-  include("ttest.jl")
-end
-
-@testset "Petri" begin
-  include("Petri.jl")
-end
-
 @testset "ModelExploration" begin
   include("ModelExploration.jl")
 end
